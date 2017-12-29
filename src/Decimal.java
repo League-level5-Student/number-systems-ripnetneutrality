372255ec474ab3ab4f66cb1c264351ccf532a15b
TheTrueTechie <shiva.malhotra.email@gmail.com>
2017-12-28 17:49:13 -0800

    Menu Fixed

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,7 +1,48 @@
-
 import java.util.Stack;
 
 public class Decimal {
+<<<<<<< HEAD
+	public String decToHex(Integer input) {
+		int temp1 = input;
+		Integer temp2;
+		String word = "";
+		Stack<String> s = new Stack<String>();
+		while (temp1 != 0) {
+			temp2 = temp1 % 16;
+
+			temp1 = temp1 / 16;
+			switch (temp2) {
+			case 10:
+				s.push("A");
+				break;
+			case 11:
+				s.push("B");
+				break;
+			case 12:
+				s.push("C");
+				break;
+			case 13:
+				s.push("D");
+				break;
+			case 14:
+				s.push("E");
+				break;
+			case 15:
+				s.push("F");
+				break;
+			default:
+				s.push(temp2 + "");
+
+			}
+			for (int i = 0; i < s.size(); i++) {
+				word = word + s.pop();
+			}
+
+		}
+		return word;
+	}
+=======
+	
 public String decToHex(Integer input) {
 	int temp1=input;
 	Integer temp2;
@@ -41,4 +82,13 @@ public String decToHex(Integer input) {
 }
 	return word;
 }
+public String decToBin(Integer input) {
+	int temp1=input;
+	Integer temp2;
+	String word= "";
+	
+	return null;
+	
+}
+>>>>>>> 41c8f522bb987b68ae4cf1001f1ad3e7c015812a
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,35 +3,53 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Menu implements ActionListener{
-	JFrame MainF = new JFrame();
-	JPanel MainP = new JPanel();
-	JButton BtD = new JButton("Bin to Dec");
-	JButton BtH = new JButton("Bin to Hex");
-	JButton DtB = new JButton("Dec to Bin");
-	JButton DtH = new JButton("Dec to Hex");
-	JButton HtB = new JButton("Hex to Bin");
-	JButton HtD = new JButton("Hex to Dec");
+	JFrame mainFrame = new JFrame();
+	JPanel mainPanel = new JPanel();
+	
+	JButton BtD = new JButton();
+	JButton BtH = new JButton();
+	JButton DtB = new JButton();
+	JButton DtH = new JButton();
+	JButton HtB = new JButton();
+	JButton HtD = new JButton();
 	
 	Decimal dec;
 	Binary bin;
 	Hexadecimal hex;
+	
+	String frame_Title = "Number Converter"; 
+	String BtD_Title = "Bin to Dec";
+	String BtH_Title = "Bin to Hex"; 
+	String DtB_Title = "Dec to Bin"; 
+	String DtH_Title = "Dec to Hex"; 
+	String HtB_Title = "Hex to Bin"; 
+	String HtD_Title = "Hex to Dec"; 
+	
 	public Menu(Binary Bin, Hexadecimal Hex, Decimal Dec) {
 		dec = Dec;
 		bin = Bin;
 		hex = Hex;
 	}
 	public void display() {
-		MainF.add(MainP);
+		mainFrame.add(mainPanel);
 		
-		MainP.add(BtD);
-		MainP.add(BtH);
-		MainP.add(DtB);
-		MainP.add(DtH);
-		MainP.add(HtB);
-		MainP.add(HtD);
+		mainPanel.add(BtD);
+		mainPanel.add(BtH);
+		mainPanel.add(DtB);
+		mainPanel.add(DtH);
+		mainPanel.add(HtB);
+		mainPanel.add(HtD);
+		
+		BtD.setText(BtD_Title);
+		BtH.setText(BtH_Title);
+		DtB.setText(DtB_Title);
+		DtH.setText(DtH_Title);
+		HtB.setText(HtB_Title);
+		HtD.setText(HtD_Title);
 		
 		BtD.addActionListener(this);
 		BtH.addActionListener(this);
@@ -39,6 +57,13 @@ public class Menu implements ActionListener{
 		DtH.addActionListener(this);
 		HtB.addActionListener(this);
 		HtD.addActionListener(this);
+		
+		mainFrame.pack();
+		mainFrame.setTitle(frame_Title);
+		mainFrame.setVisible(true);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JOptionPane.showMessageDialog(null, "Please choose a button to continue with the program.");
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
