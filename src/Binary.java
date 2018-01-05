b30c274504d1b190abcb74f65fd3ea3ce99aedf2
Cole Rudnick <league@WorthyMini-6.attlocal.net>
2018-01-04 17:51:10 -0800

    did the binary thingy
    yay

--- src/Binary.java
+++ src/Binary.java
@@ -1,30 +1,33 @@
 
 public class Binary {
-	
+
 	public int convertDec(String x) {
 		int returns = 0;
-		char[] s= x.toCharArray();
+		int final1 = 0;
+		char[] s = x.toCharArray();
 		int y = s.length;
-		
-		
-		for (int i = 0; i <s.length; i++) {
-			returns += Math.pow(Integer.parseInt(x.charAt(i)+"")*2, y-1);
-			y = y-1;
+
+		for (int i = 0; i < s.length; i++) {
+			if (x.charAt(i) == '0') {
+
+			} else {
+				returns += Math.pow(Integer.parseInt(x.charAt(i) + "") * 2, y - 1);
+			}
+			y = y - 1;
 		}
-		
 		return returns;
 	}
+
 	public int convertHex(String x) {
 		int returns = 0;
-		char[] s= x.toCharArray();
+		char[] s = x.toCharArray();
 		int y = s.length;
-		
-		
-		for (int i = 0; i <s.length; i++) {
-			returns += Math.pow(Integer.parseInt(x.charAt(i)+"")*16, y-1);
-			y = y-1;
+
+		for (int i = 0; i < s.length; i++) {
+			returns += Math.pow(Integer.parseInt(x.charAt(i) + "") * 16, y - 1);
+			y = y - 1;
 		}
-		
+
 		return returns;
 	}
 }
